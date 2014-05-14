579cacedfeb82f42f6df5b4cd8d078015ca40f73
Anatid <theduck314@gmail.com>
2014-05-13 22:35:27 -0400

    Removed spurious namespace.

--- src/AtmosphericEngine.cs
+++ src/AtmosphericEngine.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace KSPDocumentation
+/// <summary>
+/// The old, deprecated class that implemented jet engine parts before PartModules.
+/// Use an appropriately configured ModuleEngines instead of this class.
+/// </summary>
+class AtmosphericEngine : Part
 {
-    /// <summary>
-    /// The old, deprecated class that implemented jet engine parts before PartModules.
-    /// Use an appropriately configured ModuleEngines instead of this class.
-    /// </summary>
-    class AtmosphericEngine : Part
-    {
-    }
 }
+
