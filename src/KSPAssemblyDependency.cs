24790640a82b26454f7a27968199210359a70e22
Anatid <theduck314@gmail.com>
2014-08-29 00:30:58 -0400

    Added some attributes.

---
+++ src/KSPAssemblyDependency.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary>
+/// This attribute and the KSPAssembly attribute can be used to ensure that plugin assemblies are loaded in the right order.
+/// </summary>
+/// <remarks>
+/// Suppose Mod A depends on mod B, which is currently at version 2.6. 
+/// Mod B should add the following line to the end of Properties/AssemblyInfo.cs:
+/// 
+/// <code>
+/// [assembly: KSPAssembly("ModBName", 2, 6)]
+/// </code>
+///
+/// where "ModBName" is the name of Mod B (Mod B's .dll?). Replace 2 and 6 with the major and minor version of
+/// Mod B.
+/// 
+/// Then Mod A should add the following line to the end of Properties/AssemblyInfo.cs:
+/// 
+/// <code>
+/// [assembly: KSPAssemblyDependency("ModBName", 2, 6)]
+/// </code>
+/// 
+/// This will tell KSP that Mod A depends on version 2.6 of Mod B, and ensure the the assemblies get loaded in
+/// the proper order.
+/// 
+/// </remarks>
+[AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+public class KSPAssemblyDependency : Attribute
+{
+    public string name;
+    public int versionMajor;
+    public int versionMinor;
+
+    public extern KSPAssemblyDependency(string name, int versionMajor, int versionMinor);
+}
---
+++ src/KSPScenario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Like KSPAddon, but for ScenarioModules. Apply this attribute to your subclass
+/// of ScenarioModule and KSP will add it to the game at the right time.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class KSPScenario : Attribute
+{
+    public ScenarioCreationOptions createOptions;
+    public GameScenes[] tgtScenes;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="createOptions">Directions about which games the scenario should be added to. Different options
+    /// can be combined with the | operator.</param>
+    /// <param name="tgtScenes">A list of the game scenes to which the scenario should be added</param>
+    public extern KSPScenario(ScenarioCreationOptions createOptions, params GameScenes[] tgtScenes);
+
+    public extern GameScenes[] TargetScenes { get; }
+
+    public extern bool HasCreateOption(ScenarioCreationOptions option);
+}
