cae1eb5cc6bd4c56ec3eaa93b24ad9dfcf2fbba6
DMagic <david.grandy@gmail.com>
2014-09-01 23:14:06 -0400

    Three EventData classes
    I'm not sure what to call these since I can't use < > in the file names

---
+++ src/EventDataT.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A type used to generate GameEvents
+/// 
+/// Use this type with one accompanying type;
+/// GameEvents.FromToAction,
+/// GameEvents.HostedFromToAction,
+/// GameEvents.HostTargetAction
+/// are commonly used
+/// 
+/// See EventVoid for events that require no parameters
+/// or EventReport for a different style
+/// </summary>
+/// <typeparam name="T">The type to be passed through on EventData.Fire()</typeparam>
+public class EventData<T>
+{
+	/// <summary>
+	/// Generate debug logs if true
+	/// </summary>
+	public bool debugEvent;
+
+	/// <summary>
+	/// The constructor used to create a new EventData
+	/// 
+	/// EventData<Vessel> myNewEvent = new EventData<Vessel>("myNewEvent");
+	/// </summary>
+	/// <param name="eventName">Give the event a string name,
+	/// generally the same as the declared name</param>
+	public EventData(string eventName);
+
+	/// <summary>
+	/// Add a method to be run when the EventData is fired.
+	/// 
+	/// This is generally done in an object's Start or Awake method, or a class' constructor.
+	/// 
+	/// Can be setup like:
+	/// 
+	/// GameEvents.someEventDataEvent.Add(yourMethod);
+	/// 
+	/// or
+	/// 
+	/// GameEvents.someEventDataEvent.Add(new EventVoid.OnEvent(yourMethod));
+	/// </summary>
+	/// <param name="evt">The method you want to add; must contain a single parameter
+	/// of the type matching that in the host EventData</param>
+	public void Add(EventData<T>.OnEvent evt);
+	public static bool AddEventScene(string eventName, EventData<T>.OnEvent evt, bool addToAll);
+	public static bool AddEventUpwards(Transform transform, string eventName, EventData<T>.OnEvent evt, bool addToAll);
+	public static EventData<T> FindEventScene(string eventName);
+	public static List<EventData<T>> FindEventsScene(string eventName);
+	public static List<EventData<T>> FindEventsUpwards(Transform transform, string eventName);
+	public static EventData<T> FindEventUpwards(Transform transform, string eventName);
+	/// <summary>
+	/// Triggers the EventData
+	/// 
+	/// All of the methods added using Add are run after this.
+	/// </summary>
+	/// <param name="data">Single parameter matching the type of the host EventData
+	/// Use this to give information relevant to the event</param>
+	public void Fire(T data);
+	/// <summary>
+	/// Remove a method from the list of methods to be run when the EventData is fired.
+	/// 
+	/// This is generally done in an object's OnDestroy method.
+	/// </summary>
+	/// <param name="evt">The method you want to remove; must contain a single parameter
+	/// of a type matching that in the host EventData</param>
+	public void Remove(EventData<T>.OnEvent evt);
+	public static bool RemoveEventScene(string eventName, EventData<T>.OnEvent evt, bool removeFromAll);
+	public static bool RemoveEventUpwards(Transform transform, string eventName, EventData<T>.OnEvent evt, bool removeFromAll);
+
+	/// <summary>
+	/// Any methods added to the event must match the delegate's parameters;
+	/// one parameter of the given type in this case.
+	/// </summary>
+	/// <param name="data"></param>
+	public delegate void OnEvent(T data);
+}
---
+++ src/EventDataTUV.cs
@@ -0,0 +1,81 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A type used to generate GameEvents
+/// 
+/// Use this type with three accompanying parameters
+/// </summary>
+/// <typeparam name="T">The first type to passed through on EventData.Fire()</typeparam>
+/// <typeparam name="U">The second type to passed through on EventData.Fire()</typeparam>
+/// <typeparam name="V">The third type to passed through on EventData.Fire()</typeparam>
+public class EventData<T, U, V>
+{
+	/// <summary>
+	/// Generate debug logs if true
+	/// </summary>
+	public bool debugEvent;
+
+	/// <summary>
+	/// The constructor used to create a new EventData
+	/// 
+	/// EventData<Vessel, CelestialBody, String> myNewEvent = new EventData<Vessel, CelestialBody, String>("myNewEvent");
+	/// </summary>
+	/// <param name="eventName">Give the event a string name,
+	/// generally the same as the declared name</param>
+	public EventData(string eventName);
+
+	/// <summary>
+	/// Add a method to be run when the EventData is fired.
+	/// 
+	/// This is generally done in an object's Start or Awake method, or a class' constructor.
+	/// 
+	/// Can be setup like:
+	/// 
+	/// GameEvents.someEventDataEvent.Add(yourMethod);
+	/// 
+	/// or
+	/// 
+	/// GameEvents.someEventDataEvent.Add(new EventVoid.OnEvent(yourMethod));
+	/// </summary>
+	/// <param name="evt">The method you want to add; must contain three parameters
+	/// matching the types of in the host EventData</param>
+	public void Add(EventData<T, U, V>.OnEvent evt);
+	public static bool AddEventScene(string eventName, EventData<T, U, V>.OnEvent evt, bool addToAll);
+	public static bool AddEventUpwards(Transform transform, string eventName, EventData<T, U, V>.OnEvent evt, bool addToAll);
+	public static EventData<T, U, V> FindEventScene(string eventName);
+	public static List<EventData<T, U, V>> FindEventsScene(string eventName);
+	public static List<EventData<T, U, V>> FindEventsUpwards(Transform transform, string eventName);
+	public static EventData<T, U, V> FindEventUpwards(Transform transform, string eventName);
+	/// <summary>
+	/// Triggers the EventData
+	/// 
+	/// All of the methods added using Add are run after this.
+	/// </summary>
+	/// <param name="data0">A parameter matching the first type of the host EventData
+	/// Use this to give information relevant to the event</param>
+	/// <param name="data1">The second type of the host EventData</param>
+	/// <param name="data2">The third type of the host EventData</param>
+	public void Fire(T data0, U data1, V data2);
+	/// <summary>
+	/// Remove a method from the list of methods to be run when the EventData is fired.
+	/// 
+	/// This is generally done in an object's OnDestroy method.
+	/// </summary>
+	/// <param name="evt">The method you want to remove; must contain three parameters
+	/// matching the types of in the host EventData</param>
+	public void Remove(EventData<T, U, V>.OnEvent evt);
+	public static bool RemoveEventScene(string eventName, EventData<T, U, V>.OnEvent evt, bool removeFromAll);
+	public static bool RemoveEventUpwards(Transform transform, string eventName, EventData<T, U, V>.OnEvent evt, bool removeFromAll);
+
+	/// <summary>
+	/// Any methods added to the event must match the delegate's parameters;
+	/// three parameters of the given types in this case.
+	/// </summary>
+	/// <param name="data0"></param>
+	/// <param name="data1"></param>
+	/// <param name="data2"></param>
+	public delegate void OnEvent(T data0, U data1, V data2);
+}
