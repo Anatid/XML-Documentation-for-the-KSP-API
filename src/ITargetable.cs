b82e4f925edde6962f72d0e988f0b3a66ad306ab
Sebastian Krysmanski <noreply@manski.net>
2014-10-25 23:37:21 +0200

    Added some documentation related to ITargetable.

---
+++ src/ITargetable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Represents something that can be set as target while controlling
+/// a vessel. This is implemented by <see cref="Vessel"/>, <see cref="CelestialBody"/>,
+/// and <see cref="ModuleDockingNode"/>.
+/// </summary>
+public interface ITargetable
+{
+  Transform GetTransform();
+
+  Vector3 GetObtVelocity();
+
+  Vector3 GetSrfVelocity();
+
+  Vector3 GetFwdVector();
+
+  Vessel GetVessel();
+
+  /// <summary>
+  /// The name of this target.
+  /// </summary>
+  string GetName();
+
+  Orbit GetOrbit();
+
+  OrbitDriver GetOrbitDriver();
+
+  VesselTargetModes GetTargetingMode();
+}
---
+++ src/VesselTargetModes.cs
@@ -0,0 +1,22 @@
+﻿/// <summary>
+/// Describes what "properties" of a target (<see cref="ITargetable"/>) are targetable.
+/// </summary>
+public enum VesselTargetModes
+{
+  /// <summary>
+  /// None. How is this useful?
+  /// </summary>
+  None,
+  /// <summary>
+  /// Only the direction of the taget. Used by <see cref="CelestialBody"/>.
+  /// </summary>
+  Direction,
+  /// <summary>
+  /// Direction and velocity. Used by <see cref="Vessel"/>.
+  /// </summary>
+  DirectionAndVelocity,
+  /// <summary>
+  /// Direction, velocity, and orientation. Used by <see cref="ModuleDockingNode"/>.
+  /// </summary>
+  DirectionVelocityAndOrientation,
+}
