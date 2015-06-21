7e8a3d95effbaa0fb5b0c0641488c58312729b54
Bebop <plz.bea.there@gmail.com>
2015-06-21 02:48:50 +0100

    Added stub for ScreenMessage warning readers to use ScreenMessages instead.

---
+++ src/ScreenMessage.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// A class representing temporary messages posted to the screen. Don't use this class, use the static methods in ScreenMessages instead.
+/// </summary>
+public class ScreenMessage
+{
+    public float duration;
+    public GUIStyle guiStyleOverride;
+    public string message;
+    public bool persistAcrossScenes;
+    public float startTime;
+    public ScreenMessageStyle style;
+
+    public ScreenMessage(string msg, float dur, ScreenMessageStyle s);
+    public ScreenMessage(string msg, float dur, bool persistAcrossScenes, ScreenMessageStyle s);
+    public ScreenMessage(string msg, float dur, ScreenMessageStyle s, GUIStyle guiStyle);
+    public ScreenMessage(string msg, float dur, bool persistAcrossScenes, ScreenMessageStyle s, GUIStyle guiStyle);
+}
